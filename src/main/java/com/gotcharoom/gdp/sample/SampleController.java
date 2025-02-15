a4a23ee9dfc9e20dd76cfaa142ae110a9dd96612
tryoo0607 <tryoo0607@gmail.com>
2025-02-15 18:48:46 +0900

    샘플 코드 수정

--- src/main/java/com/gotcharoom/gdp/sample/SampleController.java
+++ src/main/java/com/gotcharoom/gdp/sample/SampleController.java
@@ -4,6 +4,7 @@ import com.gotcharoom.gdp.global.api.ApiResponse;
 import com.gotcharoom.gdp.global.api.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,7 +43,7 @@ public class SampleController {
             summary = "Response 테스트 - 성공 (데이터 포함)",
             description = "테스트합니다"
     )
-    @PostMapping("/response/success/data")
+    @GetMapping("/response/success/data")
     public ApiResponse<String> responseTestSuccessData() {
         return ApiResponse.success("데이터");
     }
@@ -51,7 +52,7 @@ public class SampleController {
             summary = "테스트",
             description = "테스트합니다"
     )
-    @PostMapping("/response/error")
+    @GetMapping("/response/error")
     public ApiResponse<Void> responseTestError() {
         return ApiResponse.error(ErrorResponse.TEST_FAIL);
     }
