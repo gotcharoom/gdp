d12a237b673fd254f5673af77b051628170a6f61
tryoo0607 <tryoo0607@gmail.com>
2025-02-17 11:45:33 +0900

    불필요한 내용 제거

--- src/main/java/com/gotcharoom/gdp/sample/service/SampleService.java
+++ src/main/java/com/gotcharoom/gdp/sample/service/SampleService.java
@@ -9,9 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-
 @Service
 public class SampleService {
 
