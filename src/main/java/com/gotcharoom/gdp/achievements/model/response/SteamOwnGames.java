0098acea0c0bf4a9afbd356ab2ce00b427ce8638
Asue0 <divefreedom@naver.com>
2025-03-08 16:23:34 +0900

    스팀 도전과제 불러오기 작업중(appid 불러오기 실험중)

---
+++ src/main/java/com/gotcharoom/gdp/achievements/model/response/SteamOwnGames.java
@@ -0,0 +1,17 @@
+package com.gotcharoom.gdp.achievements.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SteamOwnGames {
+    private int game_count;
+    private List<SteamOwnGameItem> games;
+}
